e04ff8788f9a8516a5829a99acc91d592359672d
Doacola <jami.lohilahti@gmail.com>
2019-02-21 20:32:54 +0200

    Hopefully fixed multilinestring, refactor to only generate required data

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
 const axios = require('axios')
 const proj4 = require('proj4')
 
-const coordinates = []
 const goddata = []
 
 const getJSON = async () => {
     return await axios.get('https://kartta.hel.fi/maps/featureloader.ashx?request=select&id=vastuualue_maka_rantojen_hoito_julkinen&resolution=8')
 }
 
-const what = async () => {
+const convertJSON = async () => {
     const heldata = await getJSON()
     const data = JSON.parse(heldata.data.slice(1))
 
@@ -18,51 +17,61 @@ const what = async () => {
         if (responsible == "Kulttuurin ja vapaa-ajan toimiala") {
             const properties = value[1].properties
             if (value[1].geometry.type == 'MultiLineString') {
-                //coordinates.push(...coords)
-                Object.entries(...coords).forEach(value => {
-                    // TODO: Fix multiline array push to get rest of the coordinates
+                const multicoords = []
+
+                Object.entries(coords).forEach(value => {
+                    const multitemp = []
+
+                    value.forEach(coord => {
+                        if (typeof coord == "object") {
+                            coord.forEach(tc => {
+                                const x = tc[0]
+                                const y = tc[1]
+                                const fincoord = converter(x, y)
+
+                                multitemp.push(fincoord)
+                            })
+                        }
+                    })
+                    multicoords.push(multitemp)
+                })
+                goddata.push({
+                    "props": properties,
+                    "geometry": {
+                        "type": value[1].geometry.type,
+                        "coordinates": multicoords
+                    }
                 })
             } else {
-                coordinates.push(...coords)
-                goddata.push({"props": properties, "coordinates": coords})
-            }
-    }
-    });
+                const convertedcoords = []
 
-    //google()
+                Object.entries(coords).forEach(coord => {
+                    const x = coord[1][0]
+                    const y = coord[1][1]
+                    const fincoord = converter(x, y)
+
+                    convertedcoords.push(fincoord)
+                })
+                goddata.push({
+                    "props": properties,
+                    "geometry": {
+                        "type": value[1].geometry.type,
+                        "coordinates": convertedcoords
+                    }
+                })
+            }
+        }
+    });
+    console.log(JSON.stringify(goddata, null, 2));
 }
 
-what()
+convertJSON()
 
-const converter2 = async (coords) => {
+const converter = (x, y) => {
     var projections = {
         fin: "+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs t",
         google: "+proj=longlat +datum=WGS84 +no_defs "
     };
 
     return proj4(projections['fin'], projections['google'], [x, y]);
-}
-
-const google = () => {
-    const fin_coords = []
-
-    coordinates.forEach((point) => {
-        const x = point[0];
-        const y = point[1];
-        fin_coords.push(converter(x, y))
-    })
-
-    fin_coords.forEach((point) => {
-        console.log(point)
-    })
-
-    //function converter(x, y) {
-    //    var projections = {
-    //        fin: "+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs t",
-    //        google: "+proj=longlat +datum=WGS84 +no_defs "
-    //    };
-    //
-    //    return proj4(projections['fin'], projections['google'], [x, y]);
-    //}
-
 }
