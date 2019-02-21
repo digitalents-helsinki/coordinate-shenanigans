721f89d9d9bc4b6d5acb16c2a9fb46aac7ddd612
Doacola <jami.lohilahti@gmail.com>
2019-02-21 16:16:19 +0200

    current progress - include props

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const axios = require('axios')
 const proj4 = require('proj4')
 
 const coordinates = []
+const goddata = []
 
 const getJSON = async () => {
     return await axios.get('https://kartta.hel.fi/maps/featureloader.ashx?request=select&id=vastuualue_maka_rantojen_hoito_julkinen&resolution=8')
@@ -13,24 +14,34 @@ const what = async () => {
 
     Object.entries(data.features).forEach(value => {
         const coords = value[1].geometry.coordinates
-        if (value[1].geometry.type == 'MultiLineString') {
-            //coordinates.push(...coords)
-            Object.entries(...coords).forEach(value => {
-                // TODO: Fix multiline array push to get rest of the coordinates
-            })
-        } else {
-            coordinates.push(...coords)
-        }
-        
+        const responsible = value[1].properties["hel:vastuu_org"]
+        if (responsible == "Kulttuurin ja vapaa-ajan toimiala") {
+            const properties = value[1].properties
+            if (value[1].geometry.type == 'MultiLineString') {
+                //coordinates.push(...coords)
+                Object.entries(...coords).forEach(value => {
+                    // TODO: Fix multiline array push to get rest of the coordinates
+                })
+            } else {
+                coordinates.push(...coords)
+                goddata.push({"props": properties, "coordinates": coords})
+            }
+    }
     });
 
-    //console.dir(coordinates, {'maxArrayLength': null});
-    console.log(coordinates.length);
-    google()
+    //google()
 }
 
 what()
 
+const converter2 = async (coords) => {
+    var projections = {
+        fin: "+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs t",
+        google: "+proj=longlat +datum=WGS84 +no_defs "
+    };
+
+    return proj4(projections['fin'], projections['google'], [x, y]);
+}
 
 const google = () => {
     const fin_coords = []
@@ -38,24 +49,20 @@ const google = () => {
     coordinates.forEach((point) => {
         const x = point[0];
         const y = point[1];
-        fin_coords.push(converter(x, y).reverse())
+        fin_coords.push(converter(x, y))
     })
 
     fin_coords.forEach((point) => {
         console.log(point)
     })
 
-    function converter(x, y) {
-        var projections = {
-            fin: "+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs t",
-            google: "+proj=longlat +datum=WGS84 +no_defs "
-        };
+    //function converter(x, y) {
+    //    var projections = {
+    //        fin: "+proj=tmerc +lat_0=0 +lon_0=25 +k=1 +x_0=25500000 +y_0=0 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs t",
+    //        google: "+proj=longlat +datum=WGS84 +no_defs "
+    //    };
+    //
+    //    return proj4(projections['fin'], projections['google'], [x, y]);
+    //}
 
-        return proj4(projections['fin'], projections['google'], [x, y]);
-    }
-
-    const url = 'https://www.google.com/maps/dir/'
-    console.log(url + fin_coords.join('/'))
-    const lgt = url + fin_coords.join('/')
-    console.log(lgt.length);
 }
